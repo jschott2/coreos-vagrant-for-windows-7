95a37fa5c25cb6c1b35dd03b5c7350afcfc08639
jschott2 <jschott@ourcio.net>
2015-09-02 11:27:09 -0400

    Fixes to user_up.sh and Vagrantfiles
    The actual fixes for Vagrantfile and user_up.sh shell script.

--- user_up.sh
+++ user_up.sh
@@ -1,9 +1,9 @@
 # user_up.sh
-# OurCIO(r) 20150901
-if  test "$1" = "" || test $1 = "/h" || test $1 = "-h" || test $1 = "--help"; then
+# OurCIO(r)
+if test $1 = "-h" || test $1 = "--help"; then
     # Give them help
     echo "user_up.sh usage and  syntax:"
-    echo " user_up.sh (/h | -h | --help) "
+    echo " user_up.sh -h | --help "
     echo "    displays this help text"
     echo " user_up.sh <username> (<password>)"
     echo "   Will create a username with the password supplied or default password"
@@ -12,14 +12,13 @@ if  test "$1" = "" || test $1 = "/h" || test $1 = "-h" || test $1 = "--help"; th
     echo "   You need at least one test user for each vmname machine for rsyncvmshare"
     echo "   to work properly."
     echo "   Default password is Password1!"
-echo ">$3<"
   else
     # perform the work
     sudo useradd  -p "*" -U -m $1 -G sudo
     if [[ ! -z "$2" ]]; then  
         echo "$1:$2" | sudo chpasswd
       else
-        echo "$1:Password1!" | sudo chpasswd
+        echo "$1:Password1!" | sudo chpass
     fi
     sudo cp -r /home/core/.ssh/authorized_keys.d/ /home/$1/.ssh/authorized_keys.d
     sudo update-ssh-keys -u $1 $1.pem
